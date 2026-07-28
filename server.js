53f42633d6eb80111fc868e4c345b6d8bd799be3
Saifudeen204 <musasadeeku@gmail.com>
2026-07-28 19:35:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const Person = require('./models/person')
 
+app.use(cors())
 app.use(express.json())
 
 app.get('/', (request, response) => {
