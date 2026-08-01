f03f28abf4311cc2095869d3e02586a3e763f67c
Saifudeen204 <musasadeeku@gmail.com>
2026-08-01 11:58:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -55,6 +55,22 @@ app.post('/api/persons', (request, response, next) => {
     .catch(error => next(error))
 })
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+app.use(errorHandler)
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
