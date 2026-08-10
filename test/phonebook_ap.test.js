0f1c6b3ac9b2d73d0ed87999d27ccdd6cf5feffe
Saifudeen204 <musasadeeku@gmail.com>
2026-08-10 07:41:35 +0100

    Update phonebook_ap.test.js

--- test/phonebook_ap.test.js
+++ test/phonebook_ap.test.js
@@ -1,1 +1,23 @@
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Person = require('../models/person')
 
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Person.deleteMany({})
+})
+
+test('persons are returned as json', async () => {
+  await api
+    .get('/api/persons')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
