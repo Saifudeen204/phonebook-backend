99eb635bf7860f5cb655a8c239dc3ebeb1531afc
Saifullahibabasaid Said <61097138-saifullahibabas@users.noreply.replit.com>
2026-08-25 14:38:17 +0000

    Close mongoose connection after tests

--- tests/person_api.test.js
+++ tests/person_api.test.js
@@ -31,6 +31,73 @@ describe('GET /api/persons', () => {
   })
 })
 
+describe('POST /api/persons', () => {
+  test('a valid person can be added', async () => {
+    const newPerson = {
+      name: 'Mary Poppendieck',
+      number: '39-23-6423122',
+    }
+
+    await api
+      .post('/api/persons')
+      .send(newPerson)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const personsAtEnd = await helper.personsInDb()
+    expect(personsAtEnd).toHaveLength(helper.initialPersons.length + 1)
+
+    const names = personsAtEnd.map(p => p.name)
+    expect(names).toContain('Mary Poppendieck')
+  })
+
+  test('person without a valid number is not added', async () => {
+    const newPerson = {
+      name: 'Missing Number Person',
+      number: 'not-a-valid-number',
+    }
+
+    await api
+      .post('/api/persons')
+      .send(newPerson)
+      .expect(400)
+
+    const personsAtEnd = await helper.personsInDb()
+    expect(personsAtEnd).toHaveLength(helper.initialPersons.length)
+  })
+
+  test('person without a name is not added', async () => {
+    const newPerson = {
+      number: '39-23-6423122',
+    }
+
+    await api
+      .post('/api/persons')
+      .send(newPerson)
+      .expect(400)
+
+    const personsAtEnd = await helper.personsInDb()
+    expect(personsAtEnd).toHaveLength(helper.initialPersons.length)
+  })
+})
+
+describe('DELETE /api/persons/:id', () => {
+  test('a person can be deleted', async () => {
+    const personsAtStart = await helper.personsInDb()
+    const personToDelete = personsAtStart[0]
+
+    await api
+      .delete(/api/persons/${personToDelete.id})
+      .expect(204)
+
+    const personsAtEnd = await helper.personsInDb()
+    expect(personsAtEnd).toHaveLength(helper.initialPersons.length - 1)
+
+    const names = personsAtEnd.map(p => p.name)
+    expect(names).not.toContain(personToDelete.name)
+  })
+})
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
